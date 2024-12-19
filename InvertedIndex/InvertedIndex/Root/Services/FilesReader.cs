67d6685d233897765a6dc04713277d280eadc574
Sabir Saiadov <sabirsayadov18@gmail.com>
2024-12-19 14:52:23 +0200

    Added: Some services for server and index

---
+++ InvertedIndex/InvertedIndex/Root/Services/FilesReader.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+
+namespace InvertedIndex.Root.Services;
+
+public partial class FilesReader
+{
+    private const string DataPath = @"C:\Users\sabir\RiderProjects\InvertedIndex\InvertedIndex\Data";
+    private List<string> _filePaths = new();
+
+    private IEnumerable<string> GetFilePaths()
+        => Directory.EnumerateFiles(DataPath, "*.*", SearchOption.AllDirectories);
+
+    public IEnumerable<(string[], string)> GetWordsFromAllFiles()
+    {
+        var filePaths = GetFilePaths();
+        foreach (var path in filePaths)
+        {
+            var cleanedPath = path.Replace(DataPath, string.Empty);
+            if (_filePaths.Contains(cleanedPath))
+                continue;
+
+            _filePaths.Add(cleanedPath);
+            var text = File.ReadAllText(path);
+            var cleanedText = WordsRegex().Replace(text, "").Split(" ");
+            yield return (cleanedText, cleanedPath);
+        }
+    }
+
+    [GeneratedRegex(@"[^a-zA-Z\s]")]
+    private static partial Regex WordsRegex();
+}
---
+++ InvertedIndex/InvertedIndex/Root/Services/ResponseFormatter.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace InvertedIndex.Root.Services;
+
+public class ResponseFormatter
+{
+    private const string DataPath = @"C:\Users\sabir\RiderProjects\InvertedIndex\InvertedIndex\Data";
+    
+    public string FormatResponse(List<string> indexes, string word)
+    {
+        var response = new StringBuilder();
+
+        foreach (var index in indexes)
+        {
+            var path = $"{DataPath}\\{index}";
+            var text = File.ReadAllText(path);
+            var sentences = text.Split(new[]{'.', '!', '?'}, StringSplitOptions.RemoveEmptyEntries);
+            
+            var matchingSentences = sentences.Where(sentence => 
+                sentence.Contains(word, StringComparison.OrdinalIgnoreCase));
+            
+            foreach (var sentence in matchingSentences)
+            {
+                var highlightedSentence = System.Text.RegularExpressions.Regex.Replace(
+                    sentence.Trim(),
+                    $@"\b{System.Text.RegularExpressions.Regex.Escape(word)}\b",
+                    match => $"<b>{match.Value}</b>",
+                    System.Text.RegularExpressions.RegexOptions.IgnoreCase
+                );
+
+                response.Append($"{index}:<br>{highlightedSentence}<br><br>");
+            }
+        }
+
+        return response.ToString();
+    }
+}
